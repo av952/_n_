fcfa316512a2ae21934e95172b481d2fe9be4651
av952 <alejandrov952@hotmail.com>
2017-01-10 22:13:15 -0500

    accion del boton de main en el fragmento habilitada

--- app/src/main/java/siono/game/android/av/siono/Level_1.java
+++ app/src/main/java/siono/game/android/av/siono/Level_1.java
@@ -1,13 +1,25 @@
 package siono.game.android.av.siono;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class Level_1 extends AppCompatActivity {
+public class Level_1 extends AppCompatActivity implements View.OnClickListener,Frag_home.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_1);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
